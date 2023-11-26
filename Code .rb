5c450acbb9febefe5c1a98b73b3c1a55b020cc27
LivKeen <148727081+LivKeen@users.noreply.github.com>
2023-11-26 16:58:55 +1100

    Add files via upload

---
+++ Code .rb
@@ -0,0 +1,396 @@
+play 36
+play 48
+sample :ambi_lunar_land
+sample :ambi_drone
+sleep 1
+play 42
+play 47
+play 36
+play 48
+play 49
+play 50
+play 30
+sleep 2
+sample :loop_amen, pan: -1
+sleep 0.877
+sample :loop_amen, pan: 1
+sample :ambi_choir, rate: 1
+sample :loop_amen, pan: 1
+sample :ambi_choir, rate: 1
+play 42
+play 47
+play 36
+play 48
+play 49
+play 50
+play 30
+play 36
+play 48
+sample :ambi_lunar_land
+sample :ambi_drone
+play 42
+play 47
+play 36
+play 48
+play 49
+play 50
+play 30
+play 36
+play 48
+sample :loop_amen, pan: -1
+sleep 0.877
+sample :loop_amen, pan: -1
+sleep 0.877
+sample :loop_amen, pan: -1
+sleep 0.877
+play 42
+play 47
+play 36
+play 48
+play 49
+play 50
+play 30
+play 36
+play 48
+sample :ambi_lunar_land
+sample :ambi_drone
+play 42
+play 47
+play 36
+play 48
+play 49
+play 50
+play 30
+play 36
+play 48
+sample :loop_amen, pan: -1
+sleep 0.877
+sample :loop_amen, pan: -1
+sleep 0.877
+sample :loop_amen, pan: -1
+sleep 0.877
+play 42
+play 47
+play 36
+play 48
+play 49
+play 50
+play 30
+play 36
+play 48
+play 42
+play 47
+play 36
+play 48
+play 42
+play 47
+play 36
+play 48
+play 42
+play 47
+play 36
+play 48
+sample :loop_amen, pan: -1
+sleep 0.877
+sample :loop_amen, pan: 1
+sample :ambi_choir, rate: 1
+sample :loop_amen, pan: 1
+sample :ambi_choir, rate: 1
+play 42
+play 36
+play 48
+sample :ambi_lunar_land
+sample :ambi_drone
+sleep 1
+play 42
+play 47
+play 36
+play 48
+play 49
+play 50
+play 30
+sleep 2
+sample :loop_amen, pan: -1
+sleep 0.877
+sample :loop_amen, pan: 1
+sample :ambi_choir, rate: 1
+sample :loop_amen, pan: 1
+sample :ambi_choir, rate: 1
+play 42
+play 47
+play 36
+play 48
+play 49
+play 50
+play 30
+play 36
+play 48
+sample :ambi_lunar_land
+sample :ambi_drone
+play 42
+play 47
+play 36
+play 48
+play 49
+play 50
+play 30
+play 36
+play 48
+sample :loop_amen, pan: -1
+sleep 0.877
+sample :loop_amen, pan: -1
+sleep 0.877
+sample :loop_amen, pan: -1
+sleep 0.877
+play 42
+play 47
+play 36
+play 48
+play 49
+play 50
+play 30
+play 36
+play 48
+sample :ambi_lunar_land
+sample :ambi_drone
+play 42
+play 47
+play 36
+play 48
+play 49
+play 50
+play 30
+play 36
+play 48
+sample :loop_amen, pan: -1
+sleep 0.877
+sample :loop_amen, pan: -1
+sleep 0.877
+sample :loop_amen, pan: -1
+sleep 0.877
+play 42
+play 47
+play 36
+play 48
+play 49
+play 50
+play 30
+play 36
+play 48
+play 42
+play 47
+play 36
+play 48
+play 42
+play 47
+play 36
+play 48
+play 42
+play 47
+play 36
+play 48
+sample :loop_amen, pan: -1
+sleep 0.877
+sample :loop_amen, pan: 1
+sample :ambi_choir, rate: 1
+sample :loop_amen, pan: 1
+sample :ambi_choir, rate: 1
+play 42
+play 36
+play 48
+sample :ambi_lunar_land
+sample :ambi_drone
+sleep 1
+play 42
+play 47
+play 36
+play 48
+play 49
+play 50
+play 30
+sleep 2
+sample :loop_amen, pan: -1
+sleep 0.877
+sample :loop_amen, pan: 1
+sample :ambi_choir, rate: 1
+sample :loop_amen, pan: 1
+sample :ambi_choir, rate: 1
+play 42
+play 47
+play 36
+play 48
+play 49
+play 50
+play 30
+play 36
+play 48
+sample :ambi_lunar_land
+sample :ambi_drone
+play 42
+play 47
+play 36
+play 48
+play 49
+play 50
+play 30
+play 36
+play 48
+sample :loop_amen, pan: -1
+sleep 0.877
+sample :loop_amen, pan: -1
+sleep 0.877
+sample :loop_amen, pan: -1
+sleep 0.877
+play 42
+play 47
+play 36
+play 48
+play 49
+play 50
+play 30
+play 36
+play 48
+sample :ambi_lunar_land
+sample :ambi_drone
+play 42
+play 47
+play 36
+play 48
+play 49
+play 50
+play 30
+play 36
+play 48
+sample :loop_amen, pan: -1
+sleep 0.877
+sample :loop_amen, pan: -1
+sleep 0.877
+sample :loop_amen, pan: -1
+sleep 0.877
+play 42
+play 47
+play 36
+play 48
+play 49
+play 50
+play 30
+play 36
+play 48
+play 42
+play 47
+play 36
+play 48
+play 42
+play 47
+play 36
+play 48
+play 42
+play 47
+play 36
+play 48
+sample :loop_amen, pan: -1
+sleep 0.877
+sample :loop_amen, pan: 1
+sample :ambi_choir, rate: 1
+sample :loop_amen, pan: 1
+sample :ambi_choir, rate: 1
+play 42
+play 36
+play 48
+sample :ambi_lunar_land
+sample :ambi_drone
+sleep 1
+play 42
+play 47
+play 36
+play 48
+play 49
+play 50
+play 30
+sleep 2
+sample :loop_amen, pan: -1
+sleep 0.877
+sample :loop_amen, pan: 1
+sample :ambi_choir, rate: 1
+sample :loop_amen, pan: 1
+sample :ambi_choir, rate: 1
+play 42
+play 47
+play 36
+play 48
+play 49
+play 50
+play 30
+play 36
+play 48
+sample :ambi_lunar_land
+sample :ambi_drone
+play 42
+play 47
+play 36
+play 48
+play 49
+play 50
+play 30
+play 36
+play 48
+sample :loop_amen, pan: -1
+sleep 0.877
+sample :loop_amen, pan: -1
+sleep 0.877
+sample :loop_amen, pan: -1
+sleep 0.877
+play 42
+play 47
+play 36
+play 48
+play 49
+play 50
+play 30
+play 36
+play 48
+sample :ambi_lunar_land
+sample :ambi_drone
+play 42
+play 47
+play 36
+play 48
+play 49
+play 50
+play 30
+play 36
+play 48
+sample :loop_amen, pan: -1
+sleep 0.877
+sample :loop_amen, pan: -1
+sleep 0.877
+sample :loop_amen, pan: -1
+sleep 0.877
+play 42
+play 47
+play 36
+play 48
+play 49
+play 50
+play 30
+play 36
+play 48
+play 42
+play 47
+play 36
+play 48
+play 42
+play 47
+play 36
+play 48
+play 42
+play 47
+play 36
+play 48
+sample :loop_amen, pan: -1
+sleep 0.877
+sample :loop_amen, pan: 1
+sample :ambi_choir, rate: 1
+sample :loop_amen, pan: 1
+sample :ambi_choir, rate: 1
+play 42
